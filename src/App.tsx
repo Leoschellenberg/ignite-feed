b1cef269177192309514926eab216bb9bc55f63b
Leonardo Martins Schellenberg <leonardomartinsschellenberg@MacBook-Pro-de-MacOs.local>
2023-05-01 18:06:22 -0300

    Create Component Sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { ThemeProvider } from "styled-components";
 
 import { DefaultTheme, GlobalStyles } from "@/styles";
 import Header from "./components/Header";
+import Sidebar from "./components/Sidebar";
+
+import * as S from "./styles/styles";
+import Post from "./components/Post";
 
 function App() {
   return (
     <ThemeProvider theme={DefaultTheme}>
       <GlobalStyles />
-      <Header />
+        <Header />
+        <S.Wrapper>
+          <Sidebar />
+          <main>
+            <Post />
+          </main>
+        </S.Wrapper>
+       
     </ThemeProvider>
   );
 }
---
+++ src/components/Post/index.tsx
@@ -0,0 +1,11 @@
+import * as S from "./styles";
+
+const Post = () => {
+  return (
+    <S.Post>
+      Post
+    </S.Post>
+  );
+};
+
+export default Post;
